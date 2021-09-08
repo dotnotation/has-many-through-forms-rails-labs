15c9c7bfea21b7372153b9b05a8cb5e150ea403e
dotnotation <dotinmotionproductions@gmail.com>
2021-09-08 17:07:32 -0400

    commit tree 42934e517cdf5173b2907b97aa1302bf3f5d44fd

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "Dorthy", email: "dotinmotion@gmail.com")
+Category.create(name: "SciFi")
+Post.create(title: "This is a post", content: "This is some content about a post.")
+PostCategory.create(post_id: 1, category_id: 1, )
+Comment.create(content: "This is a great post!", user_id: 1, post_id: 1)
+
